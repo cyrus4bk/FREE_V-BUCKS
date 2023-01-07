1534f2df1e9ed684d33085cbc9c4b77aea34c8d0
cyrus4bk <106901272+cyrus4bk@users.noreply.github.com>
2023-01-07 16:34:24 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+import os, time, secrets, random
+from pystyle import *
+
+banner = Center.XCenter(Colorate.Vertical(Colors.purple_to_blue,"""
+
+  ┌─────────────────────────────────────────┐
+  │                                         │
+  │ REJOIDN MON DISCORD discord.gg/cAJs68Ub │
+  │                                         │
+  └─────────────────────────────────────────┘
+
+"""))
+
+banner1 = Center.XCenter(Colorate.Vertical(Colors.purple_to_blue,"""
+
+
+
+  ╔═══════════════════════════════════════════════════════════════════╗
+  ║                                                 █       █         ║
+  ║   ████████    █      █ ██████    █     █ ██████ ██    ███  ██████ ║
+  ║   █           ██    ██ █    ██   █     █ █      ██  ████   █      ║
+  ║   ████         █    █  █     █   █     █ █      ██████     ██     ║
+  ║   █            ██   █  █  ███    █     █ █      ████       ████   ║
+  ║   █     █████   █  ██  █ ████    █     █ █      ████         ███  ║
+  ║   █             ██ █   █    ███  █     █ █      ██  ██         █  ║
+  ║   █              ███   █      ██ █     █ ██     ██   ██       ██  ║
+  ║   █               █    █████████ ███████ ██████ ██    ██  █████   ║
+  ║                                                                   ║
+  ║                                                                   ║
+  ║  by cyrusbk#3630                                                  ║
+  ╚═══════════════════════════════════════════════════════════════════╝
+
+
+[+] (n0mbre de nitro A generER) qye v0 us voulEZ ?
+
+> """))
+
+Screen = Center.XCenter(Colorate.Vertical(Colors.purple_to_blue,"""
+T Stupid :) ?
+
+> """))
+
+num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+hit=0
+fail=0
+os.system('cls' if os.name == 'nt' else 'clear')
+
+
+ 
+choice = int(input(banner1+" "))
+
+if choice not in [1,2]:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    input(Screen)
